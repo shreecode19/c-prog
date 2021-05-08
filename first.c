697ea4a7bb7cb2892de8edad1a1258851287f490
shreecode19 <shresthashreejan20@gmail.com>
2021-05-08 17:01:10 +0545

    first c program

---
+++ first.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main()
+{
+	printf("Asian College of Higher Studies\n");
+	printf("Shreejan Shrestha\n");
+	printf("Uniglobe ss college\n");
+	printf("3.19\n");
+
+	return 0;
+}
